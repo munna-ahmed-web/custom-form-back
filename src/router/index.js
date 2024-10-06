c0beb3aceccd6bae9cb09868f0cc9164524ed966
web.munnaahmed@gmail.com <web.munnaahmed@gmail.com>
2024-10-06 16:24:05 +0600

    controller and service updated

--- src/router/index.js
+++ src/router/index.js
@@ -10,16 +10,4 @@ router.get("/users", userController.getUsers);
 router.post("/register", userController.createUser);
 router.post("/login", authController.login);
 
-//conversation routes
-router.get("/conversation", messageController.getConversation);
-router.get("/conversation/:id", messageController.getConversationByUserId);
-router.post("/conversation", messageController.createConversation);
-
-//message routes
-router.post("/message", messageController.createMessage);
-router.get(
-  "/message/conversationId",
-  messageController.getMessageByConversationId
-);
-
 module.exports = router;
