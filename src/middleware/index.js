971ad97d14acf2f2e2c8401769679a3306634e6e
web.munnaahmed@gmail.com <web.munnaahmed@gmail.com>
2024-10-24 16:00:24 +0600

    cors updated

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 var cors = require("cors");
 
+const corsOption = {
+  origin: ["http://localhost:5173", "https://custom-form-front.onrender.com"],
+};
+
 const applyMiddleWare = (app) => {
   app.use(express.json());
-  app.use(cors());
+  app.use(cors(corsOption));
 };
 
 module.exports = applyMiddleWare;
